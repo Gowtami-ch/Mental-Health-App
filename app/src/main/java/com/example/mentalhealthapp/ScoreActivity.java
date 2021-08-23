affeb653c0ace62d172a2d1f8203da92a47cf80e
Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2021-08-23 14:49:08 +0530

    Update ScoreActivity.java

--- app/src/main/java/com/example/mentalhealthapp/ScoreActivity.java
+++ app/src/main/java/com/example/mentalhealthapp/ScoreActivity.java
@@ -11,15 +11,28 @@ import android.widget.TextView;
 public class ScoreActivity extends AppCompatActivity {
     TextView tv3;
     Button done;
+    TextView scoretext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
         tv3 = findViewById(R.id.tv3);
         done = findViewById(R.id.done);
+        scoretext=findViewById(R.id.scoretext);
         Intent intent = getIntent();
         String score = intent.getStringExtra(QuizActivity.EXTRA_SCORE);
         tv3.setText(score);
+        String status="";
+        int sc=Integer.parseInt(score);
+        if(sc>=0&&sc<15)
+          status="Low";
+        else if(sc>=15&&sc<25)
+           status="Moderate";
+        else if(sc>=25&&sc<35)
+            status="High";
+        else
+            status="Very High";
+         scoretext.setText(score+"\n"+status);
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
