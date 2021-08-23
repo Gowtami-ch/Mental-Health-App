244d0c8733e58e1b5941da7aa2a9bab9675caf64
SahithiRani <sahithirani2002@gmail.com>
2021-08-23 13:13:50 +0530

    Score Activity

--- app/src/main/java/com/example/mentalhealthapp/ScoreActivity.java
+++ app/src/main/java/com/example/mentalhealthapp/ScoreActivity.java
@@ -4,18 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
     TextView tv3;
+    Button done;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
         tv3 = findViewById(R.id.tv3);
+        done = findViewById(R.id.done);
         Intent intent = getIntent();
         String score = intent.getStringExtra(QuizActivity.EXTRA_SCORE);
         tv3.setText(score);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ScoreActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
