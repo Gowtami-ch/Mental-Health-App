f7192b4442613ade9d93a1266b7efde0053ce256
SahithiRani <sahithirani2002@gmail.com>
2021-08-28 11:03:27 +0530

    ApiClient.java created and updated

--- app/src/main/java/com/example/mentalhealthapp/ApiClient.java
+++ app/src/main/java/com/example/mentalhealthapp/ApiClient.java
@@ -11,14 +11,12 @@ public class ApiClient {
     private ApiClient(){
         retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
     }
-
     public static synchronized ApiClient getInstance(){
         if (apiClient == null){
             apiClient = new ApiClient();
         }
         return apiClient;
     }
-
 
     public ApiInterface getApi(){
         return retrofit.create(ApiInterface.class);
