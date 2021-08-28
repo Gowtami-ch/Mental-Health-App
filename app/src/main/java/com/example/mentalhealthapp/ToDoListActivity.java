cd83bc4cb79e886caf771277fd705f9dbdd5a69a
roopasree123 <73064910+roopasree123@users.noreply.github.com>
2021-08-28 23:33:45 +0530

    Add files via upload

--- app/src/main/java/com/example/mentalhealthapp/ToDoListActivity.java
+++ app/src/main/java/com/example/mentalhealthapp/ToDoListActivity.java
@@ -1,18 +1,23 @@
 package com.example.mentalhealthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,34 +27,43 @@ public class ToDoListActivity extends AppCompatActivity {
    ListView listView;
    ArrayAdapter<String> arrayAdapter;
    EditText editText;
+   Button b_save,b_load;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
-        toDoList =new ArrayList<>();
+
+
+        toDoList=PrefConfig.readListFromPref(this);
+        if(toDoList == null)
+          toDoList =new ArrayList<>();
+
         arrayAdapter =new ArrayAdapter<>(this, R.layout.list_view_layout,toDoList);
         listView = findViewById(R.id.id_list_view);
 
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 TextView textView =(TextView ) view;
                 textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
             }
         });
+
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
                 final int which_item =position;
+
                  new AlertDialog.Builder(ToDoListActivity.this)
                 .setIcon(android.R.drawable.ic_delete)
                   .setTitle("Are you sure ?")
-                         .setMessage("Doyou want to delete this item")
+                         .setMessage("Do you want to delete this item")
                          .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                              @Override
                              public void onClick(DialogInterface dialogInterface, int i) {
-                                 arrayAdapter.remove(String.valueOf(which_item));
+                                 toDoList.remove(which_item);
                                  arrayAdapter.notifyDataSetChanged();
                              }
                          })
@@ -61,10 +75,16 @@ public class ToDoListActivity extends AppCompatActivity {
         });
         editText =findViewById(R.id.id_edit_text);
     }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
     public void addItemToList(View view){
         toDoList.add(editText.getText().toString());
         arrayAdapter.notifyDataSetChanged();
 
+        PrefConfig.writeListInPref(getApplicationContext(),toDoList);
         editText.setText("");
     }
 }
+
