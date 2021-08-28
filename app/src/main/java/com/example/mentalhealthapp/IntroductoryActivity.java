91942264201d5197fce724bfcb0a69fe2b9f3be1
Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2021-08-29 00:52:53 +0530

    Update IntroductoryActivity.java

--- app/src/main/java/com/example/mentalhealthapp/IntroductoryActivity.java
+++ app/src/main/java/com/example/mentalhealthapp/IntroductoryActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class IntroductoryActivity extends AppCompatActivity
 {
+    FloatingActionButton fab;
     ImageView pinkbg;
     LottieAnimationView lottie;
     @Override
@@ -20,7 +23,14 @@ public class IntroductoryActivity extends AppCompatActivity
         lottie=findViewById(R.id.lottie);
         pinkbg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
         lottie.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
-//        Intent intent=new Intent(IntroductoryActivity.this,MainActivity.class);
-//        startActivity(intent);
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(IntroductoryActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
