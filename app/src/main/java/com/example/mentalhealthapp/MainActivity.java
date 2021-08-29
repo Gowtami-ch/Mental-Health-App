6660c8d861aaf1c938d2f262f256b498af442af6
SahithiRani <sahithirani2002@gmail.com>
2021-08-29 13:41:09 +0530

    MainActivity updated

--- app/src/main/java/com/example/mentalhealthapp/MainActivity.java
+++ app/src/main/java/com/example/mentalhealthapp/MainActivity.java
@@ -20,12 +20,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     List<Cards> list = new ArrayList<>();
     MainAdapter adapter;
-//    private Button quiz;
-//    private Button journal;
-//    private Button todolist;
-//    private Button profile;
-//    private Button news;
-//    private Button meme;
+
      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,56 +37,6 @@ public class MainActivity extends AppCompatActivity {
         }
         adapter=new MainAdapter(this,list);
         recyclerView.setAdapter(adapter);
-//        quiz=findViewById(R.id.quiz);
-//        quiz.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(MainActivity.this, "Working Fine", Toast.LENGTH_SHORT).show();
-//                Intent intent=new Intent(MainActivity.this,QuizActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        journal = findViewById(R.id.journal);
-//        journal.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(MainActivity.this, "Working Fine", Toast.LENGTH_SHORT).show();
-//                Intent intent=new Intent(MainActivity.this,JournalActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        todolist = findViewById(R.id.todolist);
-//        todolist.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(MainActivity.this, "Working Fine", Toast.LENGTH_SHORT).show();
-//                Intent intent=new Intent(MainActivity.this,ToDoListActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        profile = findViewById(R.id.profile);
-//        profile.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        meme = findViewById(R.id.meme);
-//        meme.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, MemeActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        news = findViewById(R.id.news);
-//        news.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, NewsActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+
     }
 }
