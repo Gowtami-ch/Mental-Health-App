48503e42cd1501e64700353f10e6c547cc682e80
roopasree123 <73064910+roopasree123@users.noreply.github.com>
2021-08-30 22:10:09 +0530

    Add files via upload

--- app/src/main/java/com/example/mentalhealthapp/ProfileEditActivity.java
+++ app/src/main/java/com/example/mentalhealthapp/ProfileEditActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -17,6 +18,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URI;
 
 public class ProfileEditActivity extends AppCompatActivity {
@@ -26,7 +29,9 @@ public class ProfileEditActivity extends AppCompatActivity {
     public EditText nName;
     public EditText nBio;
     public EditText nPassword;
-    public ImageView profileImage;
+    public ImageView image;
+    private static final int PICK_IMAGE =1;
+    Uri imageuri;
    // public String getString;
     public  String Name,Bio, Text;
 
@@ -40,8 +45,18 @@ public class ProfileEditActivity extends AppCompatActivity {
         nName = (EditText ) findViewById(R.id.tvNameProfile);
         nBio = (EditText ) findViewById(R.id.tvBioProfile);
         nPassword =(EditText ) findViewById(R.id.et_password_cp);
+        image=findViewById(R.id.iv_cp);
 
-        profileImage=(ImageView )findViewById(R.id.iv_cp);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallery,"Select Picture"),PICK_IMAGE);
+            }
+        });
+        //profileImage=(ImageView )findViewById(R.id.iv_cp);
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,8 +81,24 @@ public class ProfileEditActivity extends AppCompatActivity {
         });
 
     }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==PICK_IMAGE && resultCode ==RESULT_OK)
+        {
+            imageuri = data.getData();
+            try{
+                Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),imageuri);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
- //   @Override
+    }
+
+
+    //   @Override
     //protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
      //   super.onActivityResult(requestCode, resultCode, data);
        // if(requestCode==1000){
