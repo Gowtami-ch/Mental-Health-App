b62df7b26e25a346edb744d76613c7731dd46acc
Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2021-08-31 19:09:51 +0530

    Update screen1.java

--- app/src/main/java/com/example/mentalhealthapp/screen1.java
+++ app/src/main/java/com/example/mentalhealthapp/screen1.java
@@ -6,16 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class screen1 extends AppCompatActivity {
     FloatingActionButton fab1;
+    TextView skip1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen1);
+        skip1=findViewById(R.id.skip1);
+        skip1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent skip=new Intent(screen1.this, MainActivity.class);
+                startActivity(skip);
+            }
+        });
         fab1 = findViewById(R.id.fab1);
         fab1.setOnClickListener(new View.OnClickListener() {
             @Override
